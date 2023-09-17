0d98bd6c12e48403c2a55f0237bdc73ad29f2275
atharatta2@gmail.com <atharatta2@gmail.com>
2023-09-17 16:16:22 +0500

    new

--- class12.js
+++ class12.js
@@ -1,1 +1,28 @@
-"use strict";
+import inquirer from "inquirer";
+console.log("First");
+async function passingGrades() {
+    var result = await inquirer.prompt([{
+            type: "number",
+            name: "1st year",
+            message: "Enter marks",
+        }]);
+    if (result.number > 40) {
+        console.log("user is pass");
+    }
+    else if (result.number > 50) {
+        console.log("user get E grade");
+    }
+    else if (result.number > 60) {
+        console.log("user get D grade");
+    }
+    else if (result.number > 70) {
+        console.log("user get C grade");
+    }
+    else if (result.number > 80) {
+        console.log("user get B grade");
+    }
+    if (result.number > 90) {
+        console.log("user get A grade");
+    }
+}
+passingGrades();
